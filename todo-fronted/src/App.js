502c05444c28cc8bc02b80902aab91f2ff800022
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2022-06-20 13:23:45 +0600

    todo app react frontent inital commit

---
+++ todo-fronted/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: "center",
+        }}>
+        Welcome to Todo App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
