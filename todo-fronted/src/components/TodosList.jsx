6cee08dfdfb619ede9084528e3350e074771009d
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2022-06-23 16:55:28 +0600

    completed todo title add line-through text decoration

--- todo-fronted/src/components/TodosList.jsx
+++ todo-fronted/src/components/TodosList.jsx
@@ -30,7 +30,7 @@ const TodosList = () => {
           <div key={index} className="bg-gray-100 w-full rounded-sm shadow-md p-5 my-2 transition-all duration-100 hover:shadow-lg">
             <h2 className="text-black text-2xl font-bold my-2">
               <span className="text-blue-500 font-bold">Title: </span>
-              {todo.title}
+              <span className={todo.completed ? "line-through decoration-green-500" : "none"}>{todo.title}</span>
             </h2>
 
             <p className="text-black text-lg font-normal text-justify my-2">
